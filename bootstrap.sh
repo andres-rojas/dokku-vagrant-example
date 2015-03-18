dded079675f5fbfd5c1dc877b9e8fcf76da124b5
Andres Rojas <andres.rojas@mtnsat.com>
2015-03-17 21:11:00 -0400

    Quick and dirty update to v0.3.15 and Ubuntu Trusty

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 vagrant up
-vagrant ssh -- "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo DOKKU_TAG=v0.2.1 bash"
+vagrant ssh -- "wget -qO- https://raw.github.com/progrium/dokku/v0.3.15/bootstrap.sh | sudo DOKKU_TAG=v0.3.15 bash"
 
 cat ~/.ssh/id_rsa.pub | vagrant ssh -- sudo sshcommand acl-add dokku ${USER}
 
